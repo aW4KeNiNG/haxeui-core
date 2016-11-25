fda518a75bea77e67564e364f8d303017b8ed8fb
Pablo Martin <pablo@develagora.com>
2016-11-25 01:33:16 +0100

    Initial scroll management.

--- haxe/ui/containers/ScrollView.hx
+++ haxe/ui/containers/ScrollView.hx
@@ -1,5 +1,7 @@
 package haxe.ui.containers;
 
+import haxe.ui.core.Screen;
+import haxe.ui.util.Point;
 import haxe.ui.components.HScroll;
 import haxe.ui.components.VScroll;
 import haxe.ui.core.Component;
@@ -17,6 +19,8 @@ class ScrollView extends Component implements IClonable<ScrollView> {
     public var _contents:Box;
     private var _hscroll:HScroll;
     private var _vscroll:VScroll;
+
+    private var _downPos:Point;
 
     public function new() {
         super();
@@ -56,6 +60,7 @@ class ScrollView extends Component implements IClonable<ScrollView> {
     private override function createChildren():Void {
         super.createChildren();
         registerEvent(MouseEvent.MOUSE_WHEEL, _onMouseWheel);
+        registerEvent(MouseEvent.MOUSE_DOWN, _onMouseDown);
         createContentContainer();
     }
 
@@ -68,7 +73,7 @@ class ScrollView extends Component implements IClonable<ScrollView> {
             addComponent(_contents);
         }
     }
-    
+
     private override function destroyChildren():Void {
         if (_hscroll != null) {
             removeComponent(_hscroll);
@@ -244,6 +249,153 @@ class ScrollView extends Component implements IClonable<ScrollView> {
                 _vscroll.pos += 60;
             }
         }
+    }
+
+    private function _onMouseDown(event:MouseEvent) {
+        var inScroll:Bool = false;
+        if (_vscroll != null && _vscroll.hidden == false) {
+            inScroll = _vscroll.hitTest(event.screenX, event.screenY);
+        }
+        if (_hscroll != null && _hscroll.hidden == false && inScroll == false) {
+            inScroll = _hscroll.hitTest(event.screenX, event.screenY);
+        }
+        //TODO - evitar mover si ha hecho click en un componente interactivo (ver cuales son)
+//        if ( _inertialScrolling == true ) {
+//            Screen.instance.removeEventListener(Event.ENTER_FRAME, _onInertiaEnterFrame);
+//            _inertiaSpeed.x = 0;
+//            _inertiaSpeed.y = 0;
+//            _inertiaTime = Lib.getTimer();
+//        }
+
+//        var content:IDisplayObject = _container.getChildAt(0); // assume first child is content
+//        if (content != null && inScroll == false && _virtualScrolling == false) {
+//            if (content.width > layout.usableWidth || content.height > layout.usableHeight || allowPull()) {
+//                _downPos = new Point(event.stageX, event.stageY);
+//                Screen.instance.addEventListener(MouseEvent.MOUSE_UP, _onScreenMouseUp);
+//                Screen.instance.addEventListener(MouseEvent.MOUSE_MOVE, _onScreenMouseMove);
+//                dispatchEvent(new UIEvent(UIEvent.SCROLL_START));
+//            }
+//        }
+
+//        if (_virtualScrolling == true && (_vscroll != null || _hscroll != null)) {
+        if (_vscroll != null || _hscroll != null) {
+//            trace("down", event.target, event.target.id);
+            _downPos = new Point(event.screenX, event.screenY);
+            Screen.instance.registerEvent(MouseEvent.MOUSE_UP, _onScreenMouseUp);
+            Screen.instance.registerEvent(MouseEvent.MOUSE_MOVE, _onScreenMouseMove);
+//            dispatchEvent(new UIEvent(UIEvent.SCROLL_START));
+        }
+    }
+
+    private function _onScreenMouseMove(event:MouseEvent):Void {
+        if (_downPos != null) {
+            var ypos:Float = event.screenY - _downPos.y;
+            var xpos:Float = event.screenX - _downPos.x;
+
+            var target:Component = event.target;
+//            while (target != null)
+//            {
+//                xpos /= target.scaleX;
+//                ypos /= target.scaleY;
+//                target = target.parentComponent;
+//            }
+
+//            if ( _inertialScrolling == true ) {
+//                var now = Lib.getTimer();
+//                var delta = (now - _inertiaTime) / 1000; // seconds
+//                if (delta == 0) {
+//                    delta = 0.1;
+//                }
+//                _inertiaSpeed.x = _inertiaSpeed.x*0.3 + xpos*0.7 / delta;
+//                _inertiaSpeed.y = _inertiaSpeed.y*0.3 + ypos*0.7 / delta;
+//                _inertiaTime = now;
+//            }
+//_scrollSensitivity
+
+            //TODO - check scroll rect?
+            if (Math.abs(xpos) >= 0  || Math.abs(ypos) >= 0) {
+//                _eventTarget.visible = true;
+                var content:Component = _contents.getComponentAt(0); // assume first child is content
+                if (content != null) {
+                    if (xpos != 0) { // && (content.componentWidth > layout.usableWidth)) {// || _virtualScrolling == true) && _pulling == false) {
+                        if (_hscroll != null) {
+                            _hscroll.pos -= xpos;
+
+//                        if (_showHScroll == true && _autoHideScrolls == true) {
+//                            _hscroll.hidden = false;
+//                        }
+                        }
+                    }
+
+                    if (ypos != 0) { // && (content.componentHeight > layout.usableHeight)) {// || _virtualScrolling == true) && _pulling == false) {
+                        if (_vscroll != null) {
+                            _vscroll.pos -= ypos;
+
+//                        if (_showVScroll == true && _autoHideScrolls == true) {
+//                            _vscroll.hidden = false;
+//                        }
+                        }
+                    }
+
+//                    if (allowPull()) {
+//                        _refreshingView.visible = false;
+//                        _pulling = true;
+//                        content.y += ypos;
+//                        if (content.y > _refreshPromptView.height) {
+//                            content.y = _refreshPromptView.height;
+//                        } else if (content.y < 0) {
+//                            content.y = 0;
+//                        }
+//                        _refreshPromptView.alpha = content.y / _refreshPromptView.height;
+//                    }
+
+                    _downPos.x = event.screenX;
+                    _downPos.y = event.screenY;
+                }
+            }
+        }
+    }
+
+    private function allowPull() {
+//        if ((_vscroll == null || _vscroll.pos == 0) && _refreshPromptView != null) {
+//            return true;
+//        }
+        return false;
+    }
+
+    private function _onScreenMouseUp(event:MouseEvent):Void {
+
+//        if ( _inertialScrolling == true && ( Lib.getTimer() - _inertiaTime ) < 100 ) {
+//            if ( Math.abs(_inertiaSpeed.x) > _inertiaSensitivity || Math.abs(_inertiaSpeed.y) > _inertiaSensitivity )
+//                Screen.instance.addEventListener(Event.ENTER_FRAME, _onInertiaEnterFrame);
+//        }
+
+//        _eventTarget.visible = false;
+        _downPos = null;
+        Screen.instance.unregisterEvent(MouseEvent.MOUSE_UP, _onScreenMouseUp);
+        Screen.instance.unregisterEvent(MouseEvent.MOUSE_MOVE, _onScreenMouseMove);
+
+//        if (_hscroll != null) {// && _showHScroll == true && _autoHideScrolls == true) {
+//            _hscroll.visible = false;
+//        }
+//        if (_vscroll != null) {// && _showVScroll == true && _autoHideScrolls == true) {
+//            _vscroll.visible = false;
+//        }
+
+//        dispatchEvent(new UIEvent(UIEvent.SCROLL_STOP));
+
+//        if (_pulling == true && _refreshPromptView != null) {
+//            if (_refreshPromptView.alpha < 1) {
+//                _pulling = false;
+//                _refreshPromptView.alpha = 0;
+//                _refreshingView.visible = false;
+//                this.invalidate();
+//            } else {
+//                _refreshPromptView.alpha = 0;
+//                _refreshingView.visible = true;
+//                dispatchEvent(new UIEvent(UIEvent.REFRESH));
+//            }
+//        }
     }
 
     private function _onContentsResized(event:UIEvent) {
