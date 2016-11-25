e0ee4a4e16161aed64101504036fbc22f6658842
Pablo Martin <pablo@develagora.com>
2016-11-25 01:33:16 +0100

    Initial scroll management.

---
+++ haxe/ui/util/Point.hx
@@ -0,0 +1,15 @@
+package haxe.ui.util;
+
+class Point {
+    public var x(default, default):Float;
+    public var y(default, default):Float;
+
+    public function new(x:Float = 0, y:Float = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public function toString():String {
+        return '[${x},${y}]';
+    }
+}
